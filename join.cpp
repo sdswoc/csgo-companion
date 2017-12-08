9c3764bb9aefa162cdb231725fa20b9334648e34
Kanav Gupta <33966400+kanav99@users.noreply.github.com>
2017-12-08 20:13:56 +0530

    rungame() works now

--- join.cpp
+++ join.cpp
@@ -2,9 +2,9 @@
 #include<fstream>
 #include<winsock2.h>
 using namespace std;
-char ip[][16]={"127.0.0.1","192.168.1.1"};			//Contains all possible ips in lan
-int ipn=2;
-void rungame();
+char ip[][16]={"127.0.0.1","192.168.1.136"};			//Contains all possible ips in lan
+int ipn=2;												//No of ips
+void rungame(int);
 int main()
 {
 	int ch;
@@ -23,7 +23,7 @@ int main()
 		sockaddr_in servAdr;
 		servAdr.sin_family=AF_INET;
 		//servAdr.sin_addr.S_un.S_addr=inet_addr("127.0.0.1");
-		servAdr.sin_port=htons(1102);
+		servAdr.sin_port=htons(11022);
 		cout<<"Select from the following active servers>>"<<endl;
 		for(int i=0;i<ipn;i++)
 		{
@@ -31,7 +31,7 @@ int main()
 			if(connect(client,(sockaddr*)&servAdr,sizeof(servAdr))==0)
 			{
 				memset(msg,0,sizeof(msg));
-				strcpy(msg,"C");
+				strcpy(msg,"C");											//Send "C" query to fetch server name to display the list of games
 				send(client,(char*)&msg,sizeof(msg),0);
 				memset(msg,0,sizeof(msg));
 				recv(client,(char*)&msg,sizeof(msg),0);
@@ -49,24 +49,36 @@ int main()
 		if(connect(new_client,(sockaddr*)&servAdr,sizeof(servAdr))==0)
 		{
 			memset(msg,0,sizeof(msg));
-			strcpy(msg,"J");
+			strcpy(msg,"J");												//Send "J" query to join the specified server
 			send(new_client,(char*)&msg,sizeof(msg),0);
 			memset(msg,0,sizeof(msg));
 			send(new_client,(char*)&name,sizeof(name),0);
 			recv(new_client,(char*)&msg,sizeof(msg),0);
 			cout<<msg<<" is the admin"<<endl;
 			cout<<"Game is going to run";
-			rungame();
+			rungame(ch);
 		}
 		else
 		{
 			cout<<ip[ch]<<"== Server Shutdown"<<endl;
 		}
 	}
+	WSACleanup();
+	system("source.exe");
 	
 }
-void rungame()
+void rungame(int ch)
 {
-	cin.get();
-	cin.get();
+	fstream file;
+	file.open("csgo\\cfg\\config.cfg",ios::app);
+	file<<"connect \""<<ip[ch]<<":27015\"";
+	file.close();
+	cout<<"Game is running in  3..";
+	Sleep(1000);
+	cout<<"2..";
+	Sleep(1000);
+	cout<<"1..\n";
+	Sleep(1000);
+	system("loader.exe");
+	system("pause");
 }
