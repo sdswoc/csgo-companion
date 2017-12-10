fd84eb36069456e85680231f748b6e889bd0b986
Kanav Gupta <33966400+kanav99@users.noreply.github.com>
2017-12-11 00:20:58 +0530

    Fix bugs

--- headers/rungame_host.h
+++ headers/rungame_host.h
@@ -4,14 +4,13 @@
 using namespace std;
 void rungame_host()
 {
-	system("cd ..\\..");
 	cout<<"Starting the game"<<endl;
-	system("copy csgo\\cfg\\autoexec.cfg original_autoexec.cfg > dustbin");
+	system("copy ..\\..\\csgo\\cfg\\autoexec.cfg original_autoexec.cfg > dustbin");
 	fstream file;
-	file.open("csgo\\cfg\\autoexec.cfg",ios::app);
+	file.open("..\\..\\csgo\\cfg\\autoexec.cfg",ios::app);
 	file<<"sv_lan \"1\"";
 	file.close();
-	file.open("csgo\\cfg\\config.cfg",ios::app);
+	file.open("..\\..\\csgo\\cfg\\config.cfg",ios::app);
 	file<<"map \"de_dust2\"";
 	file.close();
 	cout<<"Map is de_dust2\nDont close this window directly after coming back from game."<<endl;
@@ -21,7 +20,7 @@ void rungame_host()
 	Sleep(1000);
 	cout<<"1..\n";
 	Sleep(1000);
-	system("loader.exe");
+	system("..\\..\\loader.exe");
 	system("pause");
-	system("copy original_autoexec.cfg csgo\\cfg\\autoexec.cfg > dustbin");
+	system("copy original_autoexec.cfg ..\\..\\csgo\\cfg\\autoexec.cfg > dustbin");
 }
